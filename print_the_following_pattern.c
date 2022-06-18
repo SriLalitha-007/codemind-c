cd7844e55aefdd9493834978eb204e2d3053cd26
SriLalitha-007 <96735263+SriLalitha-007@users.noreply.github.com>
2022-06-18 20:04:30 +0530

    #include<stdio.h> int main(){     int n,j,i;     scanf("%d",&n);     for (i=1;i<=n;i++){         for(j=1;j<=n;j++){             if(j==1||i==j||i==n){                 printf("*");             }             else{                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 int main(){
-    int i,j,rows;
-    scanf("%d",&rows);
-    for (i=rows;i>=1;i--){
-        for(j=1;j<=i;j++){
-            printf("%d",j);
+    int n,j,i;
+    scanf("%d",&n);
+    for (i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(j==1||i==j||i==n){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
         }
         printf("
 ");
