604c1919b6293ec10d23ddf4b05028fa144e8425
SriLalitha-007 <96735263+SriLalitha-007@users.noreply.github.com>
2022-06-21 11:05:24 +0530

    #include<stdio.h> int main(){     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++){         for(j=1;j<=n-2;j++){             printf("%d",j);         }         for(j=n-3;j>=1;j--){             printf("%d",j);         }         printf(" ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -2,14 +2,15 @@
 int main(){
     int n,i,j;
     scanf("%d",&n);
-    for (i=1;i<=n;i++)
-    {
-        for(j=n;j>=1;j--)
-        {
-            printf("%d ",j);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-2;j++){
+            printf("%d",j);
+        }
+        for(j=n-3;j>=1;j--){
+            printf("%d",j);
         }
         printf("
 ");
+        
     }
-    
 }
