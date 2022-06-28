4f1e70b51df64d632d27378877d3af1b64d6bc8b
SriLalitha-007 <96735263+SriLalitha-007@users.noreply.github.com>
2022-06-28 23:17:35 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,r,s=0,c=0,n1,n2;     scanf("%d",&n);     n1=n2=n;     c=(n==0)?1:log10(n)+1;     while(n1>0){         r=n1%10;         s=s+pow(r,c);         n1=n1/10;         c--;     }     if(n2==s){         printf("True");     }     else     printf("False");
    }

--- Disarium_number.c
+++ Disarium_number.c
@@ -1,36 +1,21 @@
-#include <stdio.h> 
-#include <math.h>
-int calculateLength(int n){    
-    int length = 0;    
-    while(n != 0){    
-        length = length + 1;    
-        n = n/10;    
-    }    
-    return length;    
-}    
-     
-int main()    
-{    
-    int num, sum = 0, rem = 0, n;    
-    scanf("%d",&num);
-    int len = calculateLength(num);    
-        
-    
-    n = num;    
-        
-    
-    while(num > 0){    
-        rem = num%10;    
-        sum = sum + (int)pow(rem,len);    
-        num = num/10;    
-        len--;    
-    }    
-        
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,r,s=0,c=0,n1,n2;
+    scanf("%d",&n);
+    n1=n2=n;
+    c=(n==0)?1:log10(n)+1;
+    while(n1>0){
+        r=n1%10;
+        s=s+pow(r,c);
+        n1=n1/10;
+        c--;
+    }
+    if(n2==s){
+        printf("True");
+    }
+    else
+    printf("False");
 
-    if(sum == n)    
-        printf("True");    
-    else    
-        printf("False");    
-     
-    return 0;    
-}    
+}
