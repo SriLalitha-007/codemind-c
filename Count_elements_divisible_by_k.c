fc7906d1f5619b1ec01d3f980380cc6f95183e60
SriLalitha-007 <96735263+SriLalitha-007@users.noreply.github.com>
2022-06-28 18:46:45 +0530

    #include<stdio.h> int main(){     int n,arr[100],c=0,i,k;     scanf("%d %d",&n,&k);     for (i=0;i<n;i++){         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++){         if(arr[i]%k==0)           c++;     }     printf("%d",c); }

---
+++ Count_elements_divisible_by_k.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,arr[100],c=0,i,k;
+    scanf("%d %d",&n,&k);
+    for (i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++){
+        if(arr[i]%k==0)
+          c++;
+    }
+    printf("%d",c);
+}
